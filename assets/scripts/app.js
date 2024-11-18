07693e8e520a1e1a7a66558307bad97615bd2ba6
ogiki477 <omojick88moses@gmail.com>
2024-11-18 15:13:22 +0300

    Operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,11 +12,22 @@
 
 //Variables And Values 
 
-let  userMessage = 'Hello World';
+// let  userMessage = 'Hello World';
 
-userMessage = "World";
+// userMessage = "World";
 
-console.log(userMessage);
-console.log(userMessage);
-console.log(userMessage);
-console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+
+
+//OPERATORS
+
+console.log(5 + 8);
+
+
+if(10 === 10){
+
+    console.log("Works");
+}
