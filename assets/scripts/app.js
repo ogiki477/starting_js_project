2e7679c3f53850f01518a6d5a3f49ca876051f0d
ogiki477 <omojick88moses@gmail.com>
2024-11-18 14:02:01 +0300

    import and export in js

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+//import {apiKey} from "./util.js";
+
+//import apiKey from "./util.js";
+
+import * as util from "./util.js";
+
+//console.log(apiKey);
+
+console.log(util.apiKey);
---
+++ assets/scripts/util.js
@@ -0,0 +1,7 @@
+//export let apiKey = "addsddsfdgdgdggdgdghjjk";
+
+export default "addsddsfdgdgdggdgdghjjk";
+
+export let apiKey  = "asnndndnbfbffbfbfbfk";
+
+export let abc = 'abbdbdbbd';
